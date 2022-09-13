5a6de79cc2399343710afa5acfb4b9dbeb2ce13b
mxj3110 <111129468+mxj3110@users.noreply.github.com>
2022-09-13 11:23:43 +0800

    Update integration_test.dart

--- test_driver/integration_test.dart
+++ test_driver/integration_test.dart
@@ -1,3 +1,7 @@
 import 'package:integration_test/integration_test_driver.dart';
 
 Future<void> main() => integrationDriver();
+  // Mock class refs
+  late ApiUser userApi;
+  late AppRoute appRoute;
+  late AppLoading appLoading;
